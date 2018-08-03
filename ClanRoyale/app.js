73ac0f5ff5a20ed76716e54143f952f8da071787
Niptide <Chris@nipti.de>
2018-08-03 17:59:44 +0200

    First prototype bot with ``!cr deserter`` functionality in a basic VS2017 Typescript Project with Express Webserver.

--- ClanRoyale/app.js
+++ ClanRoyale/app.js
@@ -1,35 +1,29 @@
-﻿require('dotenv').config();
-import debug = require('debug');
-import express = require('express');
-import path = require('path');
-
-import routes from './routes/index';
-import users from './routes/user';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
+var debug = require("debug");
+var express = require("express");
+var path = require("path");
+var index_1 = require("./routes/index");
+var user_1 = require("./routes/user");
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
+app.use('/', index_1.default);
+app.use('/users', user_1.default);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err['status'] = 404;
     next(err);
 });
-
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use((err: any, req, res, next) => {
+    app.use(function (err, req, res, next) {
         res.status(err['status'] || 500);
         res.render('error', {
             message: err.message,
@@ -37,21 +31,18 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
 // production error handler
 // no stacktraces leaked to user
-app.use((err: any, req, res, next) => {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
-
 app.set('port', process.env.PORT || 3000);
-
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
-
 require('./bot');
+//# sourceMappingURL=app.js.map
---
+++ ClanRoyale/bot.ts
@@ -0,0 +1,85 @@
+﻿var request = require('request');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const royaleApiToken = process.env.ROYAL_API_TOKEN;
+const discordBotToken = process.env.DISCORD_BOT_TOKEN;
+
+var clanTag = '2P8LCRU2';
+var msgPrefix = '!cr';
+
+console.log('start');
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+    bot.user.setActivity(`use ${msgPrefix} help`);
+});
+
+// <Message Listener>
+bot.on('message', msg => {
+    if (msg.author.bot) return; // Ignore bots.
+    if (msg.channel.type === "dm") return; // Ignore DM channels.
+    console.log('received msg');
+    if (msg.content === 'ping' || msg.content === `${msgPrefix} ping`) {
+        msg.reply('pong');
+    }
+    console.log(`Prefix incorrect? ${!msg.content.startsWith(msgPrefix)}`);
+    if (!msg.content.startsWith(msgPrefix + ' ')) return;
+    // Handle Command
+    var command = msg.content.substr(msgPrefix.length + 1);
+    console.log(`Command: ${command}`);
+    if (command === 'help') {
+        msg.reply("Always use the prefix ``" + msgPrefix + "`` when talking to me. Currently there is only the ``" + msgPrefix + " deserter`` command, which lists all war participants that did not play their wargame in last 10 clanwars.");
+    } else if (command === 'deserter') {
+        getDeserters(msg);
+    } else {
+        msg.reply("Sorry, I don't know what you are asking for. Type ``" + msgPrefix + " help`` to list available commands.");
+    }
+});
+// </Message Listener>
+
+function getDeserters(msg) {
+    var options = {
+        method: 'GET',
+        url: 'https://api.royaleapi.com/clan/' + clanTag + '/warlog',
+        headers: { auth: royaleApiToken }
+    };
+    request(options, function (error, response, body) {
+        var warlog = JSON.parse(body);
+        var embed = new Discord.RichEmbed();
+        embed.setTitle("These Players did not play their wargame in last 10 clanwars:");
+        embed.setDescription("This may happen because of several IRL-reasons, but has an massive impact on clan war result.");
+        embed.setColor(4886754);
+        embed.setTimestamp(new Date());
+        embed.setFooter("Clan Royale Bot", "https://royaleapi.github.io/cr-api-assets/badges/Coin_01.png");
+        var clanBadgeUrl;
+        for (var i in warlog) {
+            var war = warlog[i];
+            var warDate = new Date(war.createdDate * 1000);
+            var myClanStanding = war.standings.find(clan => clan.tag == clanTag);
+            if (!clanBadgeUrl) clanBadgeUrl = myClanStanding.badge.image;
+            var deserters = "";
+            for (var l in war.participants) {
+                var participant = war.participants[l];
+                if (participant.battlesPlayed == 0) {
+                    deserters += `    - [${participant.name}](https://spy.deckshop.pro/player/${participant.tag})\n`;
+                }
+            }
+            if (deserters.length == 0) {
+                deserters = " :champagne: Everyone played! :tada:";
+            }
+            embed.addField((Number(i) + 1) + `. War: ${warDate.toLocaleDateString("de-DE")} (${myClanStanding.warTrophiesChange})`, deserters);
+        }
+        embed.setThumbnail(clanBadgeUrl);
+        msg.channel.send({ embed })
+            .then(sent => console.log(`Sent a !deserter reply to ${msg.author.username}`))
+            .catch(console.error);
+    }).on('error', function (e) {
+        msg.reply("Sorry! My royaleapi.com-warlog-request failed... Try again later!");
+        console.error(e);
+    });
+}
+
+bot.login(discordBotToken);
+
+console.log('ende');
