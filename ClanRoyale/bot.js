221276cb1feac99fec1a6d0519f83bfc0d5f1682
Niptide <chris@nipti.de>
2018-08-04 20:49:05 +0200

    - Typescript 2.5 to 2.8 - improved error handling and cleaned up logging

--- ClanRoyale/bot.js
+++ ClanRoyale/bot.js
@@ -1,14 +1,13 @@
 var request = require('request');
 var Discord = require('discord.js');
 var bot = new Discord.Client();
-var royaleApiToken = process.env.ROYAL_API_TOKEN;
+var royaleApiToken = process.env.ROYALE_API_TOKEN;
 var discordBotToken = process.env.DISCORD_BOT_TOKEN;
 var clanTag = '2P8LCRU2';
-var msgPrefix = '!cr';
-console.log('start');
+var commandPrefix = '!cd';
 bot.on('ready', function () {
     console.log("Logged in as " + bot.user.tag + "!");
-    bot.user.setActivity("use " + msgPrefix + " help");
+    bot.user.setActivity("use " + commandPrefix + " help");
 });
 // <Message Listener>
 bot.on('message', function (msg) {
@@ -16,24 +15,23 @@ bot.on('message', function (msg) {
         return; // Ignore bots.
     if (msg.channel.type === "dm")
         return; // Ignore DM channels.
-    console.log('received msg');
-    if (msg.content === 'ping' || msg.content === msgPrefix + " ping") {
+    console.trace('received msg: ' + msg.content);
+    if (msg.content === 'ping' || msg.content === commandPrefix + " ping") {
         msg.reply('pong');
     }
-    console.log("Prefix incorrect? " + !msg.content.startsWith(msgPrefix));
-    if (!msg.content.startsWith(msgPrefix + ' '))
+    if (!msg.content.startsWith(commandPrefix + ' '))
         return;
     // Handle Command
-    var command = msg.content.substr(msgPrefix.length + 1);
-    console.log("Command: " + command);
+    var command = msg.content.substr(commandPrefix.length + 1);
+    console.trace("Command: " + command);
     if (command === 'help') {
-        msg.reply("Always use the prefix ``" + msgPrefix + "`` when talking to me. Currently there is only the ``" + msgPrefix + " deserter`` command, which lists all war participants that did not play their wargame in last 10 clanwars.");
+        msg.reply("Always use the prefix ``" + commandPrefix + "`` when talking to me. Currently there is only the ``" + commandPrefix + " deserter`` command, which lists all war participants that did not play their wargame in last 10 clanwars.");
     }
     else if (command === 'deserter') {
         getDeserters(msg);
     }
     else {
-        msg.reply("Sorry, I don't know what you are asking for. Type ``" + msgPrefix + " help`` to list available commands.");
+        msg.reply("Sorry, I don't know what you are asking for. Type ``" + commandPrefix + " help`` to list available commands.");
     }
 });
 // </Message Listener>
@@ -45,6 +43,11 @@ function getDeserters(msg) {
     };
     request(options, function (error, response, body) {
         var warlog = JSON.parse(body);
+        if (warlog.error) {
+            msg.reply("Sorry, there was an error requesting the Clash Royale API. Try again later.");
+            console.error("Could not access royaleapi: " + options.method + " " + options.url + "\n\t" + warlog.status + " - " + warlog.message);
+            return;
+        }
         var embed = new Discord.RichEmbed();
         embed.setTitle("These Players did not play their wargame in last 10 clanwars:");
         embed.setDescription("This may happen because of several IRL-reasons, but has an massive impact on clan war result.");
@@ -52,6 +55,10 @@ function getDeserters(msg) {
         embed.setTimestamp(new Date());
         embed.setFooter("Clan Royale Bot", "https://royaleapi.github.io/cr-api-assets/badges/Coin_01.png");
         var clanBadgeUrl;
+        if (warlog.length < 1) {
+            msg.reply("Could not get any Wars. Seems like the clan did not play any wars.");
+            return;
+        }
         for (var i in warlog) {
             var war = warlog[i];
             var warDate = new Date(war.createdDate * 1000);
@@ -80,5 +87,4 @@ function getDeserters(msg) {
     });
 }
 bot.login(discordBotToken);
-console.log('ende');
 //# sourceMappingURL=bot.js.map
--- ClanRoyale/bot.ts
+++ ClanRoyale/bot.ts
@@ -2,38 +2,35 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const royaleApiToken = process.env.ROYAL_API_TOKEN;
+const royaleApiToken = process.env.ROYALE_API_TOKEN;
 const discordBotToken = process.env.DISCORD_BOT_TOKEN;
 
 var clanTag = '2P8LCRU2';
-var msgPrefix = '!cr';
-
-console.log('start');
+var commandPrefix = '!cd';
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
-    bot.user.setActivity(`use ${msgPrefix} help`);
+    bot.user.setActivity(`use ${commandPrefix} help`);
 });
 
 // <Message Listener>
 bot.on('message', msg => {
     if (msg.author.bot) return; // Ignore bots.
     if (msg.channel.type === "dm") return; // Ignore DM channels.
-    console.log('received msg');
-    if (msg.content === 'ping' || msg.content === `${msgPrefix} ping`) {
+    console.trace('received msg: '+ msg.content);
+    if (msg.content === 'ping' || msg.content === `${commandPrefix} ping`) {
         msg.reply('pong');
     }
-    console.log(`Prefix incorrect? ${!msg.content.startsWith(msgPrefix)}`);
-    if (!msg.content.startsWith(msgPrefix + ' ')) return;
+    if (!msg.content.startsWith(commandPrefix + ' ')) return;
     // Handle Command
-    var command = msg.content.substr(msgPrefix.length + 1);
-    console.log(`Command: ${command}`);
+    var command = msg.content.substr(commandPrefix.length + 1);
+    console.trace(`Command: ${command}`);
     if (command === 'help') {
-        msg.reply("Always use the prefix ``" + msgPrefix + "`` when talking to me. Currently there is only the ``" + msgPrefix + " deserter`` command, which lists all war participants that did not play their wargame in last 10 clanwars.");
+        msg.reply("Always use the prefix ``" + commandPrefix + "`` when talking to me. Currently there is only the ``" + commandPrefix + " deserter`` command, which lists all war participants that did not play their wargame in last 10 clanwars.");
     } else if (command === 'deserter') {
         getDeserters(msg);
     } else {
-        msg.reply("Sorry, I don't know what you are asking for. Type ``" + msgPrefix + " help`` to list available commands.");
+        msg.reply("Sorry, I don't know what you are asking for. Type ``" + commandPrefix + " help`` to list available commands.");
     }
 });
 // </Message Listener>
@@ -46,6 +43,11 @@ function getDeserters(msg) {
     };
     request(options, function (error, response, body) {
         var warlog = JSON.parse(body);
+        if (warlog.error) {
+            msg.reply("Sorry, there was an error requesting the Clash Royale API. Try again later.");
+            console.error(`Could not access royaleapi: ${options.method} ${options.url}\n\t${warlog.status} - ${warlog.message}`);
+            return;
+        }
         var embed = new Discord.RichEmbed();
         embed.setTitle("These Players did not play their wargame in last 10 clanwars:");
         embed.setDescription("This may happen because of several IRL-reasons, but has an massive impact on clan war result.");
@@ -53,6 +55,10 @@ function getDeserters(msg) {
         embed.setTimestamp(new Date());
         embed.setFooter("Clan Royale Bot", "https://royaleapi.github.io/cr-api-assets/badges/Coin_01.png");
         var clanBadgeUrl;
+        if (warlog.length < 1) {
+            msg.reply("Could not get any Wars. Seems like the clan did not play any wars.");
+            return;
+        }
         for (var i in warlog) {
             var war = warlog[i];
             var warDate = new Date(war.createdDate * 1000);
@@ -74,12 +80,10 @@ function getDeserters(msg) {
         msg.channel.send({ embed })
             .then(sent => console.log(`Sent a !deserter reply to ${msg.author.username}`))
             .catch(console.error);
-    }).on('error', function (e) {
+    }).on('error', function (e) { // TODO: seems not to be called on error (e.g. bad api token)...
         msg.reply("Sorry! My royaleapi.com-warlog-request failed... Try again later!");
         console.error(e);
     });
 }
 
 bot.login(discordBotToken);
-
-console.log('ende');
